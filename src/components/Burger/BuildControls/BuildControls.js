2d769b4422a0a79378fc73436b4284ef0d91e62f
Paulo Borba <pauloesb@gmail.com>
2019-07-19 15:26:49 -0300

    Implemented show price and fixed control of ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,11 +12,16 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price: <b>{props.price.toFixed(2)}</b>
+    </p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
